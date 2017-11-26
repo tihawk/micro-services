0d8b567b7e21bf54a1c977a0b84d261c40b9c7c7
Kiril Panayotov Blagoev <cccp.hawk@gmail.com>
2017-11-26 17:21:33 +0200

    delete database credentials

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 var config = {};
 //setup database config
 config.db = {};
-config.db.host = "ds121716.mlab.com:21716/micro-services";
-config.db.usr = 'tihawk';
-config.db.pass = 'tihawk';
+config.db.host = '';
+config.db.usr = '';
+config.db.pass = '';
 
 //setup server port
 config.port = 3000;
